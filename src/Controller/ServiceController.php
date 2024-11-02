4b246d891f41d899cd5aacda6f760f19ec380aef
Gauthier Corion <43696669+MrBaguette07@users.noreply.github.com>
2024-11-02 12:39:16 +0100

    Implement service management features; add edit and delete functionality, restrict access to admin users, and update service listing layout

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class ServiceController extends AbstractController
 {
     #[Route('/services', name: 'service_list')]
-    #[IsGranted('ROLE_USER')]
+    #[IsGranted('ROLE_ADMIN')]
     public function listServices(EntityManagerInterface $entityManager): Response
     {
         $services = $entityManager->getRepository(Service::class)->findAll();
@@ -38,11 +38,57 @@ class ServiceController extends AbstractController
             $entityManager->flush();
 
             $this->addFlash('success', 'Le service a été ajouté avec succès.');
+
             return $this->redirectToRoute('service_list');
         }
 
         return $this->render('service/new.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/services/edit/{id}', name: 'edit_service')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function editService(int $id, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $service = $entityManager->getRepository(Service::class)->find($id);
+
+        if (!$service) {
+            throw $this->createNotFoundException('Service non trouvé');
+        }
+
+        $form = $this->createForm(ServiceType::class, $service);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Le service a été modifié avec succès.');
+
+            return $this->redirectToRoute('service_list');
+        }
+
+        return $this->render('service/edit.html.twig', [
+            'form' => $form->createView(),
+            'service' => $service,
+        ]);
+    }
+
+    #[Route('/services/delete/{id}', name: 'delete_service', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function deleteService(int $id, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $service = $entityManager->getRepository(Service::class)->find($id);
+
+        if (!$service) {
+            throw $this->createNotFoundException('Service non trouvé');
+        }
+
+        if ($this->isCsrfTokenValid('delete'.$service->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($service);
+            $entityManager->flush();
+            $this->addFlash('success', 'Le service a été supprimé avec succès.');
+        }
+
+        return $this->redirectToRoute('service_list');
     }
 }
